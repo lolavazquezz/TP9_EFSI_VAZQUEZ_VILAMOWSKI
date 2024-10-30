028a61157a4c376c3e76e64793a0e2cc56812966
Android Studio <devandroid@ort>
2024-10-30 09:19:39 -0300

    aas

--- frontend/app/profile/page.js
+++ frontend/app/profile/page.js
@@ -1,39 +1,70 @@
-import React from 'react';
-import Footer from "../components/footer";
-import styles from './page.module.css'; // Asumiendo que tendrás un archivo CSS para estilos
+"use client";
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import useAuth from "../components/useAuth.js";
+import Link from 'next/link';
+import styles from './page.module.css';
+import axios from 'axios';
 
-const Profile = () => {
-  // Simulamos datos del usuario para mostrar
-  const user = {
-    name: "Leila Shepard",
-    email: "leila@shepard.com",
-    profileImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK6XIfvGfED4TE4VgnFI6esbcVb2NCidLC6Zwu1K7iACEK4SfrS-n8E6R2jDBjS6J3qX4&usqp=CAU"
+function Profile() {
+  const { user, loading, showAuthOptions } = useAuth();
+  const router = useRouter();
+  const [id, setId] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser && storedUser.id) {
+      setId(storedUser.id);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    router.push("/login");
   };
 
+  if (loading) return <div>Loading...</div>;
+
+  if (showAuthOptions) {
+    return (
+      <div className={styles.authOptions}>
+        <div className={styles.buttonsContainer}>
+          <div className={styles.buttonsTop}>
+            <Link href="/login">
+              <button className={styles.button}>Log in</button>
+            </Link>
+            <Link href="/register">
+              <button className={styles.button}>Register</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-    <div className={styles.profileContainer}>
-      <header className={styles.profileHeader}>
-        <h1>Profile</h1>
-      </header>
+    <main className={styles.perfilPage}>
+      <div className={styles.profileHeader}>
+      </div>
 
-      <section className={styles.profileDetails}>
-        <div className={styles.profileImageContainer}>
-          <img src={user.profileImage} alt="Profile" className={styles.profileImage} />
+      <div className={styles.options}>
+        <div className={styles.option}>
+          <span>{user.username}</span>
         </div>
-        <div className={styles.userInfo}>
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
+        <div className={styles.option}>
+          <span>{user.first_name}</span>
         </div>
-      </section>
+        <div className={styles.option}>
+          <span>{user.last_name}</span>
+        </div>
+      </div>
 
-      <section className={styles.profileActions}>
-        <button className={styles.editProfileButton}>Log out</button>
-      </section>
-    </div>
-      <Footer className={styles.footer}/>
-    </div>
+      <button onClick={handleLogout} className={styles.logoutButton}>
+        Log out
+      </button>
+    </main>
   );
-};
+}
 
 export default Profile;
